6e6a90837245ccd7fba8843c15b605572cd89fdc
Eduardo Campos de Souza <eduardofidera@gmail.com>
2020-03-26 05:45:50 -0300

    Fix styleguide config

--- styleguide/styleguide.config.js
+++ styleguide/styleguide.config.js
@@ -3,7 +3,10 @@ const path = require("path")
 module.exports = {
   components: ["../components/**/*.js"],
   styleguideComponents: {
-    Wrapper: path.join(__dirname, "../components/ThemeWrapper/ThemeWrapper")
+    Wrapper: path.join(
+      __dirname,
+      "../components/atoms/ThemeWrapper/ThemeWrapper"
+    )
   },
   title: "Edusorcerer's Components Library",
   /** This is necessary because styleguide overrides any webpack's output config */
