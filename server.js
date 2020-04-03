88443200db8558c37489f301231dc6a994968f17
edusorcerer <eduardofidera@gmail.com>
2020-04-03 02:40:17 -0300

    Add user route

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express")
 const next = require("next")
+const session = require("express-session")
+const passport = require("passport")
+
+const userRouter = require("./server/user")
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== "production"
@@ -9,13 +13,23 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = express()
 
-  server.get("/a", (req, res) => {
-    return app.render(req, res, "/a", req.query)
-  })
+  server.use(express.json())
+  server.use(
+    session({
+      secret: "go horse mode",
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 60 * 60 * 1000, // 1 hour
+        secure: true,
+        sameSite: true
+      }
+    })
+  )
+  server.use(passport.initialize())
+  server.use(passport.session())
 
-  server.get("/b", (req, res) => {
-    return app.render(req, res, "/b", req.query)
-  })
+  server.use("/user", userRouter)
 
   server.all("*", (req, res) => {
     return handle(req, res)
