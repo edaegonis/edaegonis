b5d9871d763ba7e87aaaf10129e3ed792db43133
edusorcerer <eduardofidera@gmail.com>
2020-04-03 20:43:32 -0300

    Add API

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express")
 const next = require("next")
 const session = require("express-session")
 const passport = require("passport")
+const cookieParser = require("cookie-parser")
 
 const userRouter = require("./server/user")
+const authRouter = require("./server/auth").router
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== "production"
@@ -12,6 +14,10 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   const server = express()
+
+  server.set("trust proxy", 1)
+  server.use(express.urlencoded({ extended: false }))
+  server.use(cookieParser())
 
   server.use(express.json())
   server.use(
@@ -30,6 +36,11 @@ app.prepare().then(() => {
   server.use(passport.session())
 
   server.use("/user", userRouter)
+  server.use("/auth", authRouter)
+
+  server.post("/working", (req, res) => {
+    console.log("working")
+  })
 
   server.all("*", (req, res) => {
     return handle(req, res)
